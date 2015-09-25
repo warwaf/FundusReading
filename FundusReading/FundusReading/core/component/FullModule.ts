90f424e16b40fddd1ec205627619ff5260c68cd9
warwaf <378898280@qq.com>
2015-09-25 17:09:04 +0800

    添加新的内容

--- FundusReading/FundusReading/core/component/FullModule.ts
+++ FundusReading/FundusReading/core/component/FullModule.ts
@@ -4,42 +4,190 @@
     {
         private sw: number = screen.width;
         private sh: number = screen.height;
+        private FilterModule: JQuery;
+        private MagnifierModule: JQuery;
         constructor()
         {
-            globle.Globle.full_module.css({
-                "position": "absolute",
-                "overflow": "hidden",
-                "background": "#fff",
-                "top": 0,
-                "left": 0,
+            globle.Globle.full_module.css({               
                 "width": this.sw,
-                "height": this.sh,
-                "display":"none"
+                "height": this.sh               
             });
             this.creatTitleDiv(globle.Globle.full_module);
-            this.createLeftGlassDiv(globle.Globle.full_module);
+            this.createGlassDiv(globle.Globle.full_module);
         }
-        private createLeftGlassDiv(parent: JQuery): void
+        /**
+         *创建放大镜显示区域
+         */
+        private createGlassDiv(parent: JQuery): void
         {
-            var size = (screen.width / 2) - 2;           
-            var leftMagnifier = $('<div/>');
-            leftMagnifier.css({ "left": 0, "float": "left", "padding-top": "80px", "margin-left": "1px"})
+            var size = (screen.width / 2) - 3;                   
+            var leftMagnifier = $('<div class="full-module-left" />');          
             parent.append(leftMagnifier);
-            globle.Globle.full_left_magnifier = new war.Magnifier(leftMagnifier, size, "core/images/wangwei_20140514_141714_Color_L_037.jpg", [3216, 2136, 555, 20]);
-            var rightMagnifier = $('<div/>');
-            rightMagnifier.css({ "left": 0, "float": "left", "padding-top": "80px", "margin-left": "1px"})
-            parent.append(rightMagnifier);
-            globle.Globle.full_left_magnifier = new war.Magnifier(rightMagnifier, size, "core/images/wangwei_20140514_141714_Color_L_037.jpg", [3216, 2136, 555, 20]);
-        }
+            globle.Globle.full_left_magnifier = new war.Magnifier(leftMagnifier, size, "", [3216, 2136, 555, 20]);
 
+            leftMagnifier.css("top",(this.sh-leftMagnifier.height()-40)/2+40)
+            var rightMagnifier = $('<div class="full-module-right" />');           
+            parent.append(rightMagnifier);
+            globle.Globle.full_right_magnifier = new war.Magnifier(rightMagnifier, size, "", [3216, 2136, 555, 20]);
+            rightMagnifier.css("top", (this.sh - rightMagnifier.height() - 40) / 2+40)
+
+        }
+        /**
+         *创建title
+         */
         private creatTitleDiv(parent:JQuery)
         {
-            var title = $('<div/>');
-            title.css({
-                "height": "40px",
-                "border-bottom":"1px solid #ccc"
-            });
+            var o = this;
+            var title = $('<div class="full-module-title nav-tabs-alt"/>');
             parent.append(title);
+            var nav = $('<ul class="nav nav-tabs"></ul>');
+            var Magnifier = $('<li class=""><a>Magnifier</a></li>');
+            var Filter = $('<li class="b-r"><a>Filter</a></li>');
+            nav.append(Magnifier, Filter);
+            this.creatMagnifier(nav);
+            this.createFilter(nav);
+            title.append(nav);
+            Magnifier.click(function ()
+            {
+                if (Filter.attr("class") == "b-r")
+                {
+                    Magnifier.toggleClass("active");
+                    o.MagnifierModule.toggleClass("hide");
+                    o.MagnifierModule.toggleClass("animated fadeInRightMd");
+
+                    o.FilterModule.addClass("hide");
+                    globle.Globle.full_left_magnifier.IsShow = !globle.Globle.full_left_magnifier.IsShow;
+                    globle.Globle.full_right_magnifier.IsShow = !globle.Globle.full_right_magnifier.IsShow;
+                } else
+                {
+                    Magnifier.toggleClass("active");
+                    Filter.toggleClass("active");
+                    o.FilterModule.addClass("hide");
+                    o.MagnifierModule.removeClass("hide");
+                    o.MagnifierModule.toggleClass("animated fadeInRightMd");
+                    globle.Globle.full_left_magnifier.IsShow = globle.Globle.full_right_magnifier.IsShow = true;
+                }
+
+            });
+            Filter.click(function ()
+            {
+                if (Magnifier.attr("class") == "")
+                {
+                    Filter.toggleClass("active");
+                    o.FilterModule.toggleClass("hide");
+                    o.MagnifierModule.addClass("hide");
+                    o.FilterModule.toggleClass("animated fadeInRightMd");
+                    globle.Globle.full_left_magnifier.IsShow = globle.Globle.full_right_magnifier.IsShow = false;
+                } else
+                {
+                    Magnifier.toggleClass("active");
+                    Filter.toggleClass("active");
+                    o.MagnifierModule.addClass("hide");
+                    o.FilterModule.removeClass("hide");
+                    o.FilterModule.toggleClass("animated fadeInRightMd");
+                    globle.Globle.full_left_magnifier.IsShow = globle.Globle.full_right_magnifier.IsShow  = false;
+                }
+            });
+        }
+        /**
+        *0001
+        *创建放大镜工具栏
+        *@parent 父级   JQuery对象
+        */
+        private creatMagnifier(parent: JQuery)
+        {
+            this.MagnifierModule = $('<li class="war-v hide"></li>');
+            var nav = $('<ul class="nav nav-tabs animated fadeInRightMd"></ul>');
+            var border = $('<li><a><label>Border：</label><input type="checkbox" checked/></a></li>');
+            var multiple = $('<li><a><label>Multiple：</label><input type = "text" maxlength= "1" value="2"></a></li>');
+            var size = $('<li><a><label>Size：</label><input type = "text" maxlength= "3" value="300"></a></li>');
+            nav.append(border, multiple, size);
+            this.MagnifierModule.append(nav);
+            parent.append(this.MagnifierModule);
+            //-------------------------------------工具栏操作-------------------------------------------------
+            border.find("input[type=checkbox]").change(function ()
+            {
+                if ($(this).is(':checked'))
+                {
+                    globle.Globle.full_left_magnifier.BorderSize = globle.Globle.full_right_magnifier.BorderSize  = 1;
+                } else
+                {
+                    globle.Globle.full_left_magnifier.BorderSize = globle.Globle.full_right_magnifier.BorderSize = 0;
+                }
+            });
+            multiple.find(":text").keyup(function (e)
+            {
+                var value = $(this).val();
+                if (value > 5)
+                {
+                    value = 5;
+                    $(this).val("5");
+                }
+                if (value < 1)
+                {
+                    value = 1;
+                    $(this).val("1");
+                }
+                if      (e.which <= 49) { $(this).val("1") }
+                else if (e.which == 50) { $(this).val("2"); }
+                else if (e.which == 51) { $(this).val("3"); }
+                else if (e.which >= 52) { $(this).val("4"); }
+                globle.Globle.full_right_magnifier.multiple = globle.Globle.full_left_magnifier.multiple = $(this).val();
+            });
+            size.find(":text").keyup(function (e)
+            {
+                if      (e.which == 49) { $(this).val("100"); }
+                else if (e.which == 50) { $(this).val("200"); }
+                else if (e.which == 51) { $(this).val("300"); }
+                else if (e.which == 52) { $(this).val("400"); }
+                else                    { $(this).val("400"); }
+                globle.Globle.full_left_magnifier.MagnifierSize = globle.Globle.full_right_magnifier.MagnifierSize = $(this).val();
+            })
+        }
+        /**
+        *0001
+        *创建滤镜工具栏
+        *@parent 父级   JQuery对象
+        */
+        private createFilter(parent: JQuery)
+        {
+            this.FilterModule = $('<li class="war-v  hide"></li>');
+            var nav = $('<ul class="nav nav-tabs"></ul>');
+            var lighteness = $('<li><a><button class= "btn btn-default btn-ssm">亮度</button></a></li>');
+            var contrast = $('<li><a><button class= "btn btn-default btn-ssm">对比度</button></a></li>');
+            var green = $('<li><a><button class= "btn btn-default btn-ssm">绿色</button></a></li>');
+            var grey = $('<li><a><button class= "btn btn-default btn-ssm">灰色</button></a></li>');
+            var inverse = $('<li><a><button class= "btn btn-default btn-ssm">反色</button></a></li>');
+
+            var reset = $('<li><a><button class= "btn btn-default btn-ssm">重置</button></a></li>');
+            nav.append(lighteness, contrast, green, grey, inverse, reset);
+            this.FilterModule.append(nav);
+            parent.append(this.FilterModule);
+            //-------------------------------------工具栏操作-------------------------------------------------
+            grey.find("button").click(function ()
+            {
+                globle.Globle.full_right_magnifier.greyFilter = globle.Globle.full_left_magnifier.greyFilter = true;
+            })
+            reset.find("button").click(function ()
+            {
+                globle.Globle.full_right_magnifier.resetFilter = globle.Globle.full_left_magnifier.resetFilter = true;
+            })
+            inverse.find("button").click(function ()
+            {
+                globle.Globle.full_right_magnifier.inverseFilter = globle.Globle.full_left_magnifier.inverseFilter = true;
+            })
+            contrast.find("button").click(function ()
+            {
+                globle.Globle.full_right_magnifier.lightenessFilter = globle.Globle.full_left_magnifier.lightenessFilter = true;
+            })
+            lighteness.find("button").click(function ()
+            {
+                globle.Globle.full_right_magnifier.contrastFilter = globle.Globle.full_left_magnifier.contrastFilter = true;
+            })
+            green.find("button").click(function ()
+            {
+                globle.Globle.full_right_magnifier.greenFilter = globle.Globle.full_left_magnifier.greenFilter = true;
+            })
         }
     }
 }
